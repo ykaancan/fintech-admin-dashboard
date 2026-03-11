c147ca328a459470fa98cde196660de6e5d23323
Kaan Can Yıldırım <kaan@rodgers-almer.com>
2026-03-11 20:58:34 +0300

    chore: initialize Spring Boot project structure

---
+++ src/main/java/com/fintech/admin_dashboard/AdminDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.fintech.admin_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminDashboardApplication.class, args);
+	}
+
+}
